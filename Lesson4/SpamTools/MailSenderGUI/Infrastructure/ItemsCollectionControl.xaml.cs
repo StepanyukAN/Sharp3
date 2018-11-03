d27cc666eb86d448c487feddb1db0197bf841275
StepanyukAN <fargogi@mail.ru>
2018-11-03 19:36:44 +1000

    ДЗ-4

---
+++ Lesson4/SpamTools/MailSenderGUI/Infrastructure/ItemsCollectionControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSenderGUI.Infrastructure
+{
+    /// <summary>
+    /// Логика взаимодействия для ItemsCollectionControl.xaml
+    /// </summary>
+    public partial class ItemsCollectionControl : UserControl
+    {
+        public ItemsCollectionControl()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
---
+++ Lesson4/SpamTools/MailSenderGUI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSenderGUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnExitClick(object Sender, RoutedEventArgs E)
+        {
+            Close();
+        }
+
+        private void GoToPlannerButton_OnClick(object Sender, RoutedEventArgs E)
+        {
+            MainTabControl.SelectedItem = TimePlannerTab;
+        }
+
+        private void OnNextPressed(object Sender, EventArgs E)
+        {
+            if (MainTabControl.SelectedIndex < MainTabControl.Items.Count - 1)
+                MainTabControl.SelectedIndex++;
+        }
+
+        private void OnPrevPressed(object Sender, EventArgs E)
+        {
+            if (MainTabControl.SelectedIndex > 0)
+                MainTabControl.SelectedIndex--;
+        }
+    }
+}
